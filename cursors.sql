66e91e5a332ec17b6fb2a7bd6053ac7a642960c4
devuser290m <devuser290m@users.noreply.github.com>
2025-10-16 12:25:21 +0300

    Add SQL cursor example for AdventureWorks2022

---
+++ cursors.sql
@@ -0,0 +1,18 @@
+-- parcurgerea tuturor furnizorilor si afisarea pe rand a fiecarui nume si rating
+
+USE AdventureWorks2022
+
+DECLARE @VendorName NVARCHAR(100);
+DECLARE @CreditRating INT;
+DECLARE Cursor1 CURSOR FOR
+SELECT Name, CreditRating
+FROM Purchasing.Vendor;
+OPEN Cursor1;
+FETCH NEXT FROM Cursor1 INTO @VendorName, @CreditRating;
+WHILE @@FETCH_STATUS = 0
+BEGIN
+PRINT 'Vendor: ' + @VendorName + ' | Credit Rating: ' + CAST(@CreditRating AS NVARCHAR(10));
+FETCH NEXT FROM Cursor1 INTO @VendorName, @CreditRating;
+END
+CLOSE Cursor1;
+DEALLOCATE Cursor1;
