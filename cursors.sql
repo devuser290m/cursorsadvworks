7c342edf8e757f7b74edead26a1bc2b893a6e896
Alecs <142896209+devuser290m@users.noreply.github.com>
2025-10-16 13:22:37 +0300

    Update cursors.sql

--- cursors.sql
+++ cursors.sql
@@ -2,17 +2,17 @@
 
 USE AdventureWorks2022
 
-DECLARE @VendorName NVARCHAR(100);
-DECLARE @CreditRating INT;
+DECLARE @VendorName NVARCHAR(100)
+DECLARE @CreditRating INT
 DECLARE Cursor1 CURSOR FOR
 SELECT Name, CreditRating
-FROM Purchasing.Vendor;
-OPEN Cursor1;
-FETCH NEXT FROM Cursor1 INTO @VendorName, @CreditRating;
+FROM Purchasing.Vendor
+OPEN Cursor1
+FETCH NEXT FROM Cursor1 INTO @VendorName, @CreditRating
 WHILE @@FETCH_STATUS = 0
 BEGIN
-PRINT 'Vendor: ' + @VendorName + ' | Credit Rating: ' + CAST(@CreditRating AS NVARCHAR(10));
-FETCH NEXT FROM Cursor1 INTO @VendorName, @CreditRating;
+PRINT 'Vendor: ' + @VendorName + ' | Credit Rating: ' + CAST(@CreditRating AS NVARCHAR(10))
+FETCH NEXT FROM Cursor1 INTO @VendorName, @CreditRating
 END
 CLOSE Cursor1
 DEALLOCATE Cursor1;
